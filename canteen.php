becd4f544187f6731caee38f334897331cd4bc37
Swapanth <swapanthvakapalli@gmail.com>
2024-06-16 21:06:55 +0530

    canteen

--- canteen.php
+++ canteen.php
@@ -1,78 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-
-		<!-- Basic -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<title>Facilities</title>	
-
-		<meta name="keywords" content="WebSite Template" />
-		<meta name="description" content="Porto - Multipurpose Website Template">
-		<meta name="author" content="okler.net">
-
-		<!-- Favicon -->
-		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
-
-		<!-- Mobile Metas -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
-
-		<!-- Web Fonts  -->
-		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
-
-		<!-- Vendor CSS -->
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
-		<link rel="stylesheet" href="vendor/animate/animate.compat.css">
-		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
-		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
-
-		<!-- Theme CSS -->
-		<link rel="stylesheet" href="css/theme.css">
-		<link rel="stylesheet" href="css/theme-elements.css">
-		<link rel="stylesheet" href="css/theme-blog.css">
-		<link rel="stylesheet" href="css/theme-shop.css">
-
-		<!-- Demo CSS -->
-		<link rel="stylesheet" href="css/demos/demo-medical.css">
-
-		<!-- Skin CSS -->
-		<link id="skinCSS" rel="stylesheet" href="css/skins/skin-medical.css">
-
-		<!-- Theme Custom CSS -->
-		<link rel="stylesheet" href="css/custom.css">
-
-		<!-- Head Libs -->
-		<script src="vendor/modernizr/modernizr.min.js"></script>
-
-	</head>
-	<body>
-        
-		<div class="body">
         <?php include 'header.php'; ?>
 
 
 			<div role="main" class="main">
 
-				<section class="page-header page-header-modern page-header-md bg-transparent m-0">
-					<div class="container">
-						<div class="row">
-							
-							<div class="col-md-12 align-self-center order-1">
-								>
+			<br>
+                <br>
+                <br>
+                <br>
+                <br>
+				<section class="page-header page-header-modern page-header-md" style="background-color:#840103; height:6px; margin-Bottom:-10px;">
+					<div class="container" >
+						<div class="row" style="margin-top:-5px;">
+
+							<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+								<h1>Facilites</h1>
+								<span class="sub-title">Campus Canteen</span>
+							</div>
+							<div class="col-md-4 order-1 order-md-2 align-self-center">
+								<ul class="breadcrumb d-block text-md-end breadcrumb-light">
+									<li><a href="demo-medical.html">Home</a></li>
+									<li class="active">Facilites</li>
+								</ul>
 							</div>
 						</div>
 					</div>
 				</section>
-				<hr class="m-0">
 
 				<div class="container py-5 mt-3">
 
 					<div class="row">
+					<?php include 'Facilitiessidebar.php'; ?>
+
 						<div class="col-lg-8">
 							<div class="overflow-hidden mb-2">
 								<h2 class="font-weight-normal text-7 mb-2 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">Campus Canteen <strong class="font-weight-extra-bold"></strong></h2>
@@ -94,7 +52,6 @@
 								<div class="testimonial-content">
 									
 								</div>
-								<!-- You can add an image below -->
 								<img src="img/team/img7.jpg" class="img-fluid" alt="">
 							</div>
 						</div>
@@ -109,5 +66,3 @@
 
 	<?php include 'footer.php'; ?>
 
-	</body>
-</html>
