3f308a577c4a9cfa8f1a25f737202a9799fc2da6
Yeswanth <kolayeswanth2005@gmail.com>
2024-06-16 13:04:57 +0530

    updated the header and fotter code

--- header.php
+++ header.php
@@ -1,54 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
-	<head>
+<html lang="en" class="sticky-header-reveal">
 
-		<!-- Basic -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
 
-		<title>Sri Venkateswara College of Engineering & Technology</title>	
+	<!-- Basic -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<meta name="description" content="Sri Venkateswara College of Engineering & Technology">
-		<meta name="author" content="MCR WEB SOLUTIONS">
+	<title>Porto - Multipurpose Website Template</title>
 
-		<!-- Favicon -->
-		<link rel="shortcut icon" href="img/favicon.ico" type="img/I1" />
-		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+	<meta name="keywords" content="WebSite Template" />
+	<meta name="description" content="Porto - Multipurpose Website Template">
+	<meta name="author" content="okler.net">
 
-		<!-- Mobile Metas -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
 
-		<!-- Web Fonts  -->
-		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,25500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
 
-		<!-- Vendor CSS -->
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
-		<link rel="stylesheet" href="vendor/animate/animate.compat.css">
-		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
-		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+	<!-- Web Fonts  -->
+	<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
 
-		<!-- Theme CSS -->
-		<link rel="stylesheet" href="css/theme.css">
-		<link rel="stylesheet" href="css/theme-elements.css">
-		<link rel="stylesheet" href="css/theme-blog.css">
-		<link rel="stylesheet" href="css/theme-shop.css">
+	<!-- Vendor CSS -->
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+	<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
 
-		<!-- Current Page CSS -->
-		<link rel="stylesheet" href="vendor/circle-flip-slideshow/css/component.css">
+	<!-- Theme CSS -->
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/theme-elements.css">
+	<link rel="stylesheet" href="css/theme-blog.css">
+	<link rel="stylesheet" href="css/theme-shop.css">
 
-		<!-- Skin CSS -->
-		<link id="skinCSS" rel="stylesheet" href="css/skins/default.css">
+	<!-- Current Page CSS -->
+	<link rel="stylesheet" href="vendor/circle-flip-slideshow/css/component.css">
 
-		<!-- Theme Custom CSS -->
-		<link rel="stylesheet" href="css/custom.css">
+	<!-- Skin CSS -->
+	<link id="skinCSS" rel="stylesheet" href="css/skins/default.css">
 
-		<!-- Head Libs -->
-		<script src="vendor/modernizr/modernizr.min.js"></script>
-
-		<style>
+	<!-- Theme Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
+	<style>
 		.dropdown-menu {
             min-width: 200px; /* Adjust the minimum width as needed */
         }
@@ -65,61 +63,61 @@
         }
     </style>
 
-	</head>
-	<body data-plugin-page-transition>
+</head>
 
-		<div class="body">
-			<header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
-				<div class="header-body border-top-0" >
-				<div class="header-top header-top-default border-bottom-0"  style="background-color: #3ab3ed;">
-						<div class="container"  style="background-color: #3ab3ed; width: 100%;">
-							<div class="header-row py-2">
-								<div class="header-column justify-content-start"  style="background-color: #3ab3ed;">
-									<div class="header-row">
-										<nav class="header-nav-top">
-											<ul class="nav nav-pills">
-												<li class="nav-item"  style="background-color: #3ab3ed;">
-													<strong style="color: white; ">COLLEGE CODE FOR EAMCET / ECET / PGCET - VETS</strong>
-												</li>
-											</ul>
-										</nav>
-									</div>
-								</div>
-								<div class="header-column justify-content-end">
-									<div class="header-row">
-										<nav class="header-nav-top">
-											<ul class="nav nav-pills text-uppercase text-2">
-												<li class="nav-item nav-item-anim-icon d-none d-md-block">
-													<a class="nav-link ps-0 text-light opacity-7" href="about-us.html"><i class="fas fa-angle-right"></i> About Us</a>
-												</li>
-												<li class="nav-item nav-item-anim-icon d-none d-md-block">
-													<a class="nav-link text-light opacity-7 pe-0" href="contact-us.html"><i class="fas fa-angle-right"></i> Contact Us</a>
-												</li>
-											</ul>
-										</nav>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="header-container container">
-						
-						<div class="header-row">
-							<div class="header-column">
+<body data-plugin-page-transition>
+
+	<div class="body">
+		<header id="header" class="header-effect-reveal" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'reveal', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 200, 'stickySetTop': '-52px'}">
+			<div class="header-body border-top-0 header-body-bottom-border">
+				<div class="header-top" style="background-color: #3ab3ed;">
+					<div class="container" style="background-color: #3ab3ed; width: 100%;">
+						<div class="header-row py-1">
+							<div class="header-column justify-content-start">
 								<div class="header-row">
-									<div class="header-logo header-logo-sticky-change" style="width: 82px; height: 40px;margin-Top:-19px;" >
-										<a href="index.html">
-											<img class="header-logo-non-sticky opacity-0" alt="Porto" width="100" height="100" src="img/I1.png" style="margin-Top:-12px;" >
-											<img class="header-logo-sticky opacity-0" alt="Porto" width="170" height="100px" src="img/I2.png" style="margin-bottom: 20px;">
-										</a>
-									</div>
+									<nav class="header-nav-top">
+										<ul class="nav nav-pills">
+											<li class="nav-item" style="background-color: #3ab3ed;">
+												<strong style="color: white; ">COLLEGE CODE FOR EAMCET / ECET / PGCET - VETS</strong>
+											</li>
+										</ul>
+									</nav>
 								</div>
 							</div>
 							<div class="header-column justify-content-end">
 								<div class="header-row">
-									<div class="header-nav header-nav-links">
-									<div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-dropdown-arrow header-nav-main-dropdown-center header-nav-main-effect-3 header-nav-main-sub-effect-1">
-									<nav class="collapse">
+									<nav class="header-nav-top">
+										<ul class="nav nav-pills text-uppercase text-2">
+											<li class="nav-item nav-item-anim-icon d-none d-md-block">
+												<a class="nav-link ps-0 text-light opacity-7" href="about-us.html"><i class="fas fa-angle-right"></i> About Us</a>
+											</li>
+											<li class="nav-item nav-item-anim-icon d-none d-md-block">
+												<a class="nav-link text-light opacity-7 pe-0" href="contact-us.html"><i class="fas fa-angle-right"></i> Contact Us</a>
+											</li>
+										</ul>
+									</nav>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="header-container header-container-height-sm container">
+					<div class="header-row">
+						<div class="header-column">
+							<div class="header-row">
+								<div class="header-logo header-logo-sticky-change" style="width: 82px; height: 40px;margin-Top:-19px;">
+									<a href="index.html">
+										<img class="header-logo-non-sticky opacity-0" alt="Porto" width="100" height="100" src="img/I1.png" style="margin-Top:-12px;">
+										<img class="header-logo-sticky opacity-0" alt="Porto" width="170" height="100px" src="img/I2.png" style="margin-bottom: 20px;">
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="header-column justify-content-end">
+							<div class="header-row">
+								<div class="header-nav header-nav-line header-nav-top-line header-nav-top-line-with-border order-2 order-lg-1">
+									<div class="header-nav-main header-nav-main-square header-nav-main-effect-2 header-nav-main-sub-effect-1">
+										<nav class="collapse">
 											<ul class="nav nav-pills" id="mainNav">
 												<li class="active">
 													<a href="index.php">
@@ -172,7 +170,7 @@
 														<li><a href="soon.php">EVENTS GALLERY</a></li>
 													</ul>
 												</li>
-											
+
 												<li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">FACILITES</a>
 													<ul class="dropdown-menu">
 														<li><a href="soon.php">CENTRAL LIBRARY</a></li>
@@ -187,34 +185,29 @@
 														<li><a href="soon.php">COLLABORATION</a></li>
 													</ul>
 												</li>
-										
+
 												<li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">P & T</a>
 													<ul class="dropdown-menu">
 														<li><a href="soon.php">PLACEMENT & TRANNING</a></li>
 														<li><a href="soon.php">COMPETATIVE EXAMINATIONS</a></li>
 														<li><a href="soon.php">WEB PORTAL</a></li>
 														<li><a href="soon.php">PLACEMENT & CONTACT DETAILS</a></li>
-														<li><a href="soon.php">JOB RECURTORS</a></li>	
+														<li><a href="soon.php">JOB RECURTORS</a></li>
 													</ul>
 												</li>
 												<li class="dropdown"><a class="" href="#">CONTACT</a></li>
-													
+
 											</ul>
-											</nav>
-										</div>
-										<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
-											<i class="fas fa-bars"></i>
-										</button>
+										</nav>
 									</div>
-									
+									<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+										<i class="fas fa-bars"></i>
+									</button>
 								</div>
+
 							</div>
 						</div>
 					</div>
 				</div>
-			</header>
-
-			
-
-			
-		</div>
+			</div>
+		</header>
