718e8c8367b6c4bbff0ba5a9acfcf3e705ff9327
Tejaswicsd <tejaswiguttula1@gmail.com>
2024-06-17 00:12:03 +0530

    complete

--- Facilitiessidebar.php
+++ Facilitiessidebar.php
@@ -3,16 +3,16 @@
 
 								<h5 class="font-weight-semi-bold">Categories</h5>
 								<ul class="nav nav-list flex-column mb-5">
-									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="">CENTRAL LIBRARY</a></li>
-									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Hostels.php">HOSTEL</a></li>
+									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Library">CENTRAL LIBRARY</a></li>
+									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">HOSTEL</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Transport.php">TRANSPORT</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_ESVCET">ESVCET</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Internet.php">INTERNET</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Canteen.php">CANTEEN</a></li>
-									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Enterpre.php">ENTERPRENUERSHIP CELL</a></li>
+									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">ENTERPRENUERSHIP CELL</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Medical.php">MEDICAL</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Sportsandcultures.php">SPORTS & CULTURALS</a></li>
-                                    <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Collaboration.php">COLLABORATION</a></li>
+                                    <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">COLLABORATION</a></li>
 
 
 								</ul>
