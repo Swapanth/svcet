768230a8831ec114c1ae6faf50e8287abf7a8972
Manasavilluri <167791436+Manasavilluri@users.noreply.github.com>
2024-06-17 11:49:27 +0530

    IQAC

--- Activitiessidebar.php
+++ Activitiessidebar.php
@@ -4,7 +4,7 @@
 								<h5 class="font-weight-semi-bold">Categories</h5>
 								<ul class="nav nav-list flex-column mb-5">
 									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="NSS.php">NSS</a></li>
-									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">IQAC</a></li>
+									<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="IQAC.php">IQAC</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="alumini.php">ALUMINI</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">PROFESSIONAL BODIES</a></li>
                                     <li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">STUDENT CHAPTERS</a></li>
