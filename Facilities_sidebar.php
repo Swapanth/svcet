bb0acb3c70921d67def946315ad76502682d7f52
Tejaswicsd <tejaswiguttula1@gmail.com>
2024-06-17 14:30:59 +0530

    sidebar

--- Facilities_sidebar.php
+++ Facilities_sidebar.php
@@ -4,15 +4,15 @@
 		<h5 class="font-weight-semi-bold">Categories</h5>
 		<ul class="nav nav-list flex-column mb-5">
 			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Library">Central Library</a></li>
-			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">Hostel</a></li>
+			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Hostels">Hostel</a></li>
 			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Transport.php">Transport</a></li>
 			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_ESVCET">Esvcet</a></li>
 			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Internet.php">Internet</a></li>
 			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Canteen.php">Canteen</a></li>
-			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">Enterpreneurship Cell</a></li>
+			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Enterpre">Enterpreneurship Cell</a></li>
 			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Medical.php">Medical</a></li>
 			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Sportsandcultures.php">Sports & Culturals</a></li>
-			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="soon.php">Collaboration</a></li>
+			<li class="nav-item"><a class="nav-link" data-hash data-hash-offset="0" data-hash-offset-lg="95" href="Facilites_Collaboration">Collaboration</a></li>
 		</ul>
 
 
