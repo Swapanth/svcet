353f645356f39de2546f34269ebfb4f240fbaffa
Manasavilluri <167791436+Manasavilluri@users.noreply.github.com>
2024-06-19 18:33:49 +0530

    mech

--- mech.php
+++ mech.php
@@ -1,98 +1,125 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>Mechanical Engineering Department Profile</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            line-height: 1.6;
-            margin: 20px;
-        }
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            text-align: justify;
-            
-        }
-        .header {
-            text-align: center;
-            font-weight: 1000;
-            font-size: 32px;
-            margin-bottom: -300px;
-            
-        }
-        .content {
-            font-size: 18px;
-        }
-        .all-content {
-            margin-top: 2px;
-            margin-left: 80px;
-            
-  
-        }
-        .col-md-3 {
-            width: 30%;
-            margin-top: 180px;
-            margin-left: 10px;
-            margin-right: 10px;
-            height: 100%;
-        }
-        .row {
-            display: flex;
-            flex-wrap: wrap;
-        }
-    </style>
-</head>
-<body> 
-    <div class="row">
-        <?php include "mechmenu.php"; ?>
-    </div>  
-    
-    <div role="main" class="main">
-        <div class="container">
-            <div class="all-content">
-                <div class="header"></div>
-                <div class="content">
-                    <h2 style="text-align :center; font-weight: 500;">ABOUT DEPARTMENT</h2>
-                    <h2>About Mechanical Engineering</h2>
-                    <p>Mechanical engineering is a discipline of engineering that applies the principles of physics and materials science for analysis, design, manufacturing, and maintenance of mechanical systems. It is the branch of engineering that involves the production and usage of heat and mechanical power for the design, production, and operation of machines and tools. It is one of the oldest and broadest engineering disciplines. The engineering field requires an understanding of core concepts including mechanics, kinematics, thermodynamics, materials science, and structural analysis. Mechanical engineers use these core principles along with tools like computer-aided engineering and product lifecycle management to design and analyze manufacturing plants, industrial equipment and machinery, heating and cooling systems, transport systems, aircraft, watercraft, robotics, medical devices and more.</p>
-                    
-                    <p>Mechanical engineers today are pursuing developments in fields such as composites, mechatronics, and nanotechnology. Mechanical engineering overlaps with aerospace engineering, civil engineering, electrical engineering, petroleum engineering, and chemical engineering to varying amounts.</p>
-                    
-                    <p>Like other career paths, entry into this particular branch of engineering also requires certain basic traits. First and foremost, you must have a penchant for mechanical components and machinery and how these machines work. You must be good at physics, chemistry, and mathematics. You must also have skills such as an analytical bent of mind, logical reasoning, and problem solving.</p>
+	<head>
 
-                    <h2>Scope</h2>
-                    <p>Mechanical Engineers are required in all manufacturing facilities. The working criteria of a mechanical engineer changes according to the type and domain of the company they are working with and the area of specialization. In a broader sense, it can be said that a mechanical engineer works on the design and control of a system that goes into the process of manufacturing the machinery and product. He tests new systems for feasibility and efficiency and carries out quality management and improvement processes.</p>
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-                    <p>There is tremendous scope for mechanical engineers in automobile engineering, cement industry, steel, power sector, hydraulics, manufacturing plants, drilling and mining industry, petroleum, aeronautical, biotechnology, and many more. Nowadays they are also increasingly needed in the environmental and bio-medical fields.</p>
-                    
-                    <p>A beginner in Mechanical Engineering can opt for various job openings such as: Maintenance Engineer, Safety Engineer, Quality Assurance, CNC Programmer, Jr. Engineer, Design Engineer, CAD/CAM Trainer, Production Supervisor/Engineer, R&D Trainee, etc.</p>
+		<title>Mechanical Department</title>	
+
+		<meta name="keywords" content="WebSite Template" />
+		<meta name="description" content="Porto - Multipurpose Website Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+
+		<!-- Demo CSS -->
+		<link rel="stylesheet" href="css/demos/demo-medical.css">
+
+		<!-- Skin CSS -->
+		<link id="skinCSS" rel="stylesheet" href="css/skins/skin-medical.css">
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.min.js"></script>
+
+	</head>
+	<body>
+        
+		<div class="body">
+        <?php include 'header.php'; ?>
+
+			<div role="main" class="main">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+				<section class="page-header page-header-modern page-header-md" style="background-color:#840103; height:6px; margin-Bottom:-10px;">
+                <div class="container">
+                    <div class="row" style="margin-top:-5px;">
+
+                        <div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+                            <h1>Mechanical Engineering</h1>
+                            <span class="sub-title">
+                            Academics
+                            </span>
+                        </div>
+                        <div class="col-md-4 order-1 order-md-2 align-self-center">
+                            <ul class="breadcrumb d-block text-md-end breadcrumb-light">
+                                <li><a href="index.php">Home</a></li>
+                                <li class="active">Admissions</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+            </section>
+                 
+				<div class="container">
 
-            </div>
+					<div class="row mt-5 mb-4">
+                    <?php include 'mechmenu.php'; ?>
+						<div class="col">
+                            
+                            <h2 class="font-weight-semibold mb-3">DEPARTMENT PROFILE</h2>
+                            <p>The Department of Mechanical Engineering has been in established since the college started in year 2009. Department is affiliated to JNTUGV Vizianagaram since 2022 and approved by AICTE New Delhi. The Department is proud to have a vibrant student fraternity who pursue Undergraduate and Post-graduate courses. The annual intake of students is 60+12 in the undergraduate course and 18 in Post graduate course.  The Department has facilities in terms of faculty, infrastructure and equipment. The Department has a team of diversely qualified faculty of 14 including Professors,   Associate Professors and Assistant Professors, who aims at delivering quality lectures that blends with their rich research experience. 
+Numerous research papers have been published in National and International Journals and Conferences. The Department arranges guest lectures by eminent professors from reputed institutions and experts from industry to bridge the gap between academics and industry. Faculty members are encouraged to improve their academic qualification and to gain experience by attending FDPs, workshops, conferences etc. They are also encouraged to present research papers at workshops and conferences, and to participate in co-curricular and extracurricular activities.
+</p>
+<p>The Department promotes active industry-institute collaboration by organizing workshops and by taking part in sponsored research projects and consultancy services. Visits to the industries for faculty and students are frequently arranged to enhance the practical exposure with the real corporate world. Department also organizes programmers like Alumni interaction sessions with current students and make them familiar with specific market requirements and demands.</p>                        
 
-        </div>
+						</div>
+					</div>
 
-        <?php include 'footer.php'; ?>
-    </div>
+				</div>
 
-    <!-- Vendor -->
-    <script src="vendor/plugins/js/plugins.min.js"></script>
+			</div>
 
-    <!-- Theme Base, Components and Settings -->
-    <script src="js/theme.js"></script>
+			<?php include 'footer.php'; ?>
+		</div>
 
-    <!-- Current Page Vendor and Views -->
-    <script src="js/views/view.contact.js"></script>
+		<!-- Vendor -->
+		<script src="vendor/plugins/js/plugins.min.js"></script>
 
-    <!-- Demo -->
-    <script src="js/demos/demo-medical.js"></script>
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
 
-    <!-- Theme Custom -->
-    <script src="js/custom.js"></script>
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
 
-    <!-- Theme Initialization Files -->
-    <script src="js/theme.init.js"></script>
+		<!-- Demo -->
+		<script src="js/demos/demo-medical.js"></script>
 
-</body>
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+	</body>
 </html>
